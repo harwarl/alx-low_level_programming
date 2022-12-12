c01c73289b5653c14dad8cb05d3ef48761d78aad
harwarl <harwarl87@gmail.com>
2022-12-12 12:32:57 -0800

    task 5 done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main- To check if a number is positive or negative or zero
+ *
+ * Return: 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		if (n == 103 || n == 101)
+			continue;
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
