71e45202d0129c3934e9a4c3420f67e17cd0c1fa
harwarl <harwarl87@gmail.com>
2022-12-12 13:32:11 -0800

    all done

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - to print zero to ten
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
 
+	for (n = 97; n <= 122; n++)
+	{
+		putchar(n);
+		putchar('\n');
+	}
+	return (0);
+}
