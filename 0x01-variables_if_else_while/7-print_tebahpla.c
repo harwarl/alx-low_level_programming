e8c061f8dedb83ecf40de470e91c1eff7f31a93b
harwarl <harwarl87@gmail.com>
2022-12-12 13:44:17 -0800

    almost done

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - to reverse letters
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 122; n >= 97; n--)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
