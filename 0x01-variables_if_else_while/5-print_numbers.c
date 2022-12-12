43ca1df8646efef58a0bef04a8985a4f6a70cb60
harwarl <harwarl87@gmail.com>
2022-12-12 12:39:54 -0800

    balck panther

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main- To check if a number is positive or negative or zero
+ *
+ * Return: 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 0; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
