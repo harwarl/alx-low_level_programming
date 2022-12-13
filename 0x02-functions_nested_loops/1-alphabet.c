c452ab848cae79a4107f1262bd435a792c0ef6f4
harwarl <harwarl87@gmail.com>
2022-12-13 14:58:33 -0800

    all 2 done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - to print all letters alphabetically
+ *
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		_putchar('n');
+	}
+	_putchar('\n');	
+}
