119c40c19982de1fcd80a702963c7c617c51b76d
harwarl <harwarl87@gmail.com>
2022-12-14 08:48:30 -0800

    all done

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - to check if it contains some alphabet
+ * @c: letter to be checked
+ * Return: 1 if true otherwise 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
