ef43e2a38814cae14faa3e240b691a9366846224
harwarl <harwarl87@gmail.com>
2022-12-20 17:47:36 -0800

    task 6 done

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2- function name
+ * @str: entered string
+ * Return: to return even numbers
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
