2e2e7354e90e63bb3567ccdd4fc607e465da3321
harwarl <harwarl87@gmail.com>
2022-12-20 18:23:06 -0800

    task 7 done

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts_half - puts half of a string
+ * @str: the string
+ * Return half of a string
+ */
+void puts_half(char *str)
+{
+	int i = 0, j;
+
+	while (str[i] != '\0')
+		i++;
+	j = i / 2;
+	if (i % 2 != 0)
+		j = (i - 1) / 2;
+	while (j >= 0)
+	{
+		_putchar(str[i - j]);
+		j--;
+	}
+	_putchar('\n');
+}
